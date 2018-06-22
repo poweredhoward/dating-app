8b27cef83194f8581befb87d67f818fde0e01be5
mhoward <matthew.howard06@yahoo.com>
2018-06-21 18:23:52 -0700

    Matching logic working

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,11 +2,18 @@ function Friend(name, photo, scores){
     this.name = name;
     this.photo = photo;
     this.scores = scores;
-    this.sum = sumScores(this.scores);
+    //this.sum = sumScores(this.scores);
     
 };
+//DUMMY DATA GOES HERE
 
-var friends = [];
+var friends = [
+    new Friend("Matthew", "1.png", [1,2,3,4,5,5,4,3,2,1]),
+    new Friend("Mike", "2.png", [1,2,3,4,4,4,4,3,2,1]),
+    new Friend("Justin", "3.png", [3,2,3,4,5,5,4,3,2,3]),
+    new Friend("Chloe", "4.png", [3,4,5,4,5,5,4,3,2,1]),
+    new Friend("Dummy", "5.png", [2,2,2,2,2,2,2,2,2,2])
+];
 
 module.exports = {
     Friend,
--- app/server.js
+++ app/server.js
@@ -3,6 +3,8 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var friend = require("./data/friends.js").Friend;
 var friends = require("./data/friends.js").friends;
+// console.log(friends);
+
 
 var PORT = process.env.PORT || 3000;
 
@@ -12,40 +14,56 @@ var best_match = {
 };
 
 
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/survey", (req,res)=>{
     res.sendFile(path.join(__dirname, "public/survey.html"));
 });
 
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
+
 
 app.get("/api/friends", (req,res)=>{
-    // res.json(friends);
-    res.json(JSON.stringify(friends));
+    res.json(friends);
 });
 
 app.post("/api/friends", (req,res)=>{
-    var f = req.params.friend;
+    var f = req.body.friend;
+    var newperson = new friend(f.name, f.photo, f.answers);
+    console.log(newperson);
 
     
     for(var i=0 ; i<friends.length ; i++){
         var difference = 0;
-        for(var j=0 ; j<friends[i].length ; j++){
-            difference += Math.abs(friends[i].scores[j] - f.scores[j]);
+        for(var j=0 ; j<friends[i].scores.length ; j++){
+            difference += Math.abs(friends[i].scores[j] - newperson.scores[j]);
+            // console.log("Difference: " + difference);
+
         }
-        if(best_match !== null && difference < best_match.difference){
+        if(best_match.friend === null){
+            best_match.friend = friends[i];
+            best_match.difference = difference;
+        }
+        else if(difference < best_match.difference){
             best_match.friend = friends[i];
             best_match.difference = difference;
         }
     }
 
-    friends.push(f);
+    friends.push(newperson);
+    console.log(best_match);
     res.json(best_match.friend);//GIVE RESULT
+    //res.redirect("/survey");
 });
 
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 app.listen(PORT, function(){
     console.log("App listening on port " + PORT);
 })
